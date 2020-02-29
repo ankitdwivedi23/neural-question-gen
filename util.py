015a720a37f40df82d9d84e07107722c6f639aba
Arunothia Marappan <armarapp@microsoft.com>
2020-02-28 22:32:38 -0800

    Started modifying code for QG test using beam search

--- util.py
+++ util.py
@@ -543,66 +543,21 @@ def torch_from_json(path, dtype=torch.float32):
 
     return tensor
 
-
-def discretize(p_start, p_end, max_len=15, no_answer=False):
-    """Discretize soft predictions to get start and end indices.
-
-    Choose the pair `(i, j)` of indices that maximizes `p1[i] * p2[j]`
-    subject to `i <= j` and `j - i + 1 <= max_len`.
+def beamSearch(p, beam_size=3):
+    """Discretize soft predictions to get question text.
 
     Args:
-        p_start (torch.Tensor): Soft predictions for start index.
-            Shape (batch_size, context_len).
-        p_end (torch.Tensor): Soft predictions for end index.
-            Shape (batch_size, context_len).
-        max_len (int): Maximum length of the discretized prediction.
-            I.e., enforce that `preds[i, 1] - preds[i, 0] + 1 <= max_len`.
-        no_answer (bool): Treat 0-index as the no-answer prediction. Consider
-            a prediction no-answer if `preds[0, 0] * preds[0, 1]` is greater
-            than the probability assigned to the max-probability span.
+        p (torch.Tensor): Soft predictions for question word index.
+            Shape (batch_size, question_len, vocab_size).
+        beam_size (Int): size of beam
 
     Returns:
-        start_idxs (torch.Tensor): Hard predictions for start index.
-            Shape (batch_size,)
-        end_idxs (torch.Tensor): Hard predictions for end index.
-            Shape (batch_size,)
+        pred_questions (torch.Tensor): Questions word indices predicted by beam Search.
+            Shape (batch_size, question_len)
     """
-    if p_start.min() < 0 or p_start.max() > 1 \
-            or p_end.min() < 0 or p_end.max() > 1:
-        raise ValueError('Expected p_start and p_end to have values in [0, 1]')
 
-    # Compute pairwise probabilities
-    p_start = p_start.unsqueeze(dim=2)
-    p_end = p_end.unsqueeze(dim=1)
-    p_joint = torch.matmul(p_start, p_end)  # (batch_size, c_len, c_len)
 
-    # Restrict to pairs (i, j) such that i <= j <= i + max_len - 1
-    c_len, device = p_start.size(1), p_start.device
-    is_legal_pair = torch.triu(torch.ones((c_len, c_len), device=device))
-    is_legal_pair -= torch.triu(torch.ones((c_len, c_len), device=device),
-                                diagonal=max_len)
-    if no_answer:
-        # Index 0 is no-answer
-        p_no_answer = p_joint[:, 0, 0].clone()
-        is_legal_pair[0, :] = 0
-        is_legal_pair[:, 0] = 0
-    else:
-        p_no_answer = None
-    p_joint *= is_legal_pair
-
-    # Take pair (i, j) that maximizes p_joint
-    max_in_row, _ = torch.max(p_joint, dim=2)
-    max_in_col, _ = torch.max(p_joint, dim=1)
-    start_idxs = torch.argmax(max_in_row, dim=-1)
-    end_idxs = torch.argmax(max_in_col, dim=-1)
-
-    if no_answer:
-        # Predict no-answer whenever p_no_answer > max_prob
-        max_prob, _ = torch.max(max_in_col, dim=-1)
-        start_idxs[p_no_answer > max_prob] = 0
-        end_idxs[p_no_answer > max_prob] = 0
-
-    return start_idxs, end_idxs
+    return pred_questions 
 
 
 def convert_tokens(eval_dict, qa_id, y_start_list, y_end_list, no_answer):
