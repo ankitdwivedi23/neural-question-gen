7dcb8650020cfa06bd14187daacdf3002440e581
Ankit Dwivedi <adwivedi@microsoft.com>
2020-03-02 19:23:45 -0800

    use NLL to save best model and add more logging

--- args.py
+++ args.py
@@ -115,7 +115,7 @@ def get_train_args():
                         help='Probability of zeroing an activation in dropout layers.')
     parser.add_argument('--metric_name',
                         type=str,
-                        default='PPL',
+                        default='NLL',
                         choices=('NLL', 'PPL'),
                         help='Name of dev metric to determine best checkpoint.')
     parser.add_argument('--max_checkpoints',
