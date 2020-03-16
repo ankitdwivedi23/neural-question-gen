30829f7a755441406c4ae155351370674057157b
Ankit Dwivedi <adwivedi@microsoft.com>
2020-03-16 15:30:55 -0700

    remove extra import

--- transformer/layers.py
+++ transformer/layers.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from util import masked_softmax
 
 
 class Embedding(nn.Module):
