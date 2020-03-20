c62d56039db86bdc16c227dbbacf0ddeeccee8d3
Ankit Dwivedi <adwivedi@microsoft.com>
2020-03-20 12:56:32 -0700

    print input to pack_padded_sequence

--- seq2seq/layers.py
+++ seq2seq/layers.py
@@ -104,6 +104,8 @@ class EncoderRNN(nn.Module):
         # Sort by length and pack sequence for RNN
         lengths, sort_idx = lengths.sort(0, descending=True)
         x = x[sort_idx]     # (batch_size, seq_len, input_size)
+        print("Sorted:")
+        print(x)
         x = pack_padded_sequence(x, lengths, batch_first=True)
         print("Packed Sequence")
         print(x)
