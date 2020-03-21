ceaf5babdeee7644af23b80929a03913f63d36e3
Ankit Dwivedi <adwivedi@microsoft.com>
2020-03-20 17:46:38 -0700

    enable pack_padded_sequence on unsorted input, again

--- seq2seq/layers.py
+++ seq2seq/layers.py
@@ -104,13 +104,13 @@ class EncoderRNN(nn.Module):
         # Sort by length and pack sequence for RNN
         #print("Before sorting:")
         #print(x)
-        lengths, sort_idx = lengths.sort(0, descending=True)
+        #lengths, sort_idx = lengths.sort(0, descending=True)
         #print("Lengths shape:")
         #print(lengths.shape)
-        x = x[sort_idx]     # (batch_size, seq_len, input_size)
+        #x = x[sort_idx]     # (batch_size, seq_len, input_size)
         #print("Sorted:")
         #print(x)
-        x = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=True)
+        x = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
         #print("Packed Sequence")
         #print(x)
 
@@ -122,8 +122,8 @@ class EncoderRNN(nn.Module):
 
         # Unpack and reverse sort
         x, _ = pad_packed_sequence(x, batch_first=True, total_length=orig_len)
-        _, unsort_idx = sort_idx.sort(0)
-        x = x[unsort_idx]   # (batch_size, seq_len, 2 * hidden_size)
+        #_, unsort_idx = sort_idx.sort(0)
+        #x = x[unsort_idx]   # (batch_size, seq_len, 2 * hidden_size)
 
         # Apply dropout (RNN applies dropout after all but the last layer)
         #enc_hiddens = F.dropout(x, self.drop_prob, self.training)
