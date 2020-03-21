79e7c98d1bc0abf69c6cd6082217af9ce76fffe4
Ankit Dwivedi <adwivedi@microsoft.com>
2020-03-21 13:36:28 -0700

    default type float

--- seq2seq/train.py
+++ seq2seq/train.py
@@ -55,7 +55,7 @@ def percentile(t: torch.tensor, q: float) -> Union[int, float]:
     return result
 
 def main(args):    
-    torch.set_default_dtype(torch.float64)
+    #torch.set_default_dtype(torch.float64)
 
     # Set up logging and devices
     args.save_dir = util.get_save_dir(args.save_dir, args.name, training=True)
